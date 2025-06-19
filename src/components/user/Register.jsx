4c722fec6e5cb8b3646ec872ddb83e1496dc0398
Shibu-Patil <patilshibu@gmail.com>
2025-06-19 13:34:57 +0530

    made page responsive

--- src/components/user/Register.jsx
+++ src/components/user/Register.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { CgNametag } from "react-icons/cg";
 import { CgRename } from "react-icons/cg";
 import { MdAlternateEmail } from "react-icons/md";
@@ -7,32 +7,49 @@ import { MdEventRepeat } from "react-icons/md";
 
 
 const Register = () => {
+  let [formData,setFormData]=useState({
+    userName:"",
+    password:"",
+    name:"",
+    email:""
+  })
+
+  let handelChange=e=>{
+    let {name,value}=e.target
+    setFormData((preVal)=>({...preVal,[name]:value}))
+  }
+
+  let handelSubmit=e=>{
+    e.preventDefault()
+    console.log(formData);
+    
+  }
   return (
     <div className='bg-[#efefef] size-full flex justify-center items-center'>
-      <form action="" className='w-1/2  h-3/4 rounded-3xl bg-white shadow-2xl flex justify-center items-center flex-col gap-4 px-[80px] py-40'>
+      <form action="" className='w-1/2 h-[90%]  rounded-3xl bg-white shadow-2xl flex  items-center flex-col gap-8 px-[80px] py-20 max-sm:w-[90%]' onSubmit={handelSubmit}>
         <div className='font-bold w-full flex justify-center items-center'>
-          <h1 className='text-3xl'>Registration Form</h1>
+          <h1 className='text-3xl max-lg:text-sm'>Registration Form</h1>
         </div>
 
         <div className='border-2  w-full flex justify-center items-center px-3 rounded-sm'>
-          <input type="text" name="" placeholder='Enter Name' className='w-full outline-none px-4 h-10' />
+          <input type="text" name="name" placeholder='Enter Name' className='w-full outline-none px-4 h-10'  onChange={handelChange}/>
           <span><CgNametag /></span>
         </div>
 
          <div className='border-2  w-full flex justify-center items-center px-3 rounded-sm'>
-          <input type="text" name="" placeholder='Enter Username' className='w-full outline-none px-4 h-10'/>
+          <input type="text" name="userName" placeholder='Enter Username' className='w-full outline-none px-4 h-10' onChange={handelChange}/>
             <span><CgRename /></span>
         </div>
 
 
         <div className='border-2  w-full flex justify-center items-center px-3 rounded-sm'>
-          <input type="email" name="" placeholder='Enter Email' className='w-full outline-none px-4 h-10'/>
+          <input type="email" name="email" placeholder='Enter Email' className='w-full outline-none px-4 h-10' onChange={handelChange}/>
             <span><MdAlternateEmail /></span>
         </div>
 
 
         <div className='border-2  w-full flex justify-center items-center px-3 rounded-sm'>
-          <input type="password" name="" placeholder='Enter password' className='w-full outline-none px-4 h-10'/>
+          <input type="password" name="password" placeholder='Enter password' className='w-full outline-none px-4 h-10' onChange={handelChange}/>
             <span><RiLockPasswordLine /></span>
         </div>
 
